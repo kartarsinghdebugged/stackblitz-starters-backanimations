dc483ced3d29f835a32172dd8de1cddcc770b727
kartarsinghdebugged <kartar.singh@debugged-pro.com>
2024-03-14 17:35:43 +0530

    initial commit

---
+++ app/test-route/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+
+const Page = () => {
+    return(
+        <>
+            <h1>Heloo page ...</h1>
+            <Link href={'/'}>Home</Link>
+        </>
+    )
+
+}
+
+export default Page
